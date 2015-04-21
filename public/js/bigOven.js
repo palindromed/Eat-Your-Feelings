d4e3c66fe3aaeb43be5874c0eac7230af0fa1bf8
Hannah Krager <hannahkrager@gmail.com>
2015-04-21 13:11:01 -0700

    get all objects from API call into array

--- public/js/bigOven.js
+++ public/js/bigOven.js
@@ -1,51 +1,35 @@
-$(document).ready(function() {
-
-   var recipesArray =[];
-       var renderImages = function(recipes) {
-    recipes.forEach(function (image) {
-      recipesArray.push(new Recipe(image.WebURL))
-    });
- }
+var recipeArray = [];
 
 function getRecipeJson(searchTerm) {
-        var apiKey = "dvxTzcHziZpKgfz9rxpuA9i3Qh10wNK3";
-        var titleKeyword = '"'+ searchTerm +'"';
-        var url = "http://api.bigoven.com/recipes?pg=1&rpp=25&title_kw="
+
+  // var Recipe = function(info){
+  //    this.name = info,
+  //    this.image = info.ImageURL
+  //    this.
+  // }
+
+  var apiKey = "dvxTzcHziZpKgfz9rxpuA9i3Qh10wNK3";
+  var titleKeyword = '"' + searchTerm +'"';
+  var url = "http://api.bigoven.com/recipes?pg=1&rpp=25&title_kw="
                   + titleKeyword
                   + "&api_key="+apiKey;
 
-         $.ajax({
-            type: "GET",
-            dataType: 'json',
-            cache: false,
-            url: url,
-            success: function (data) {
-                alert('success');
-                console.log(data.Results);
-                renderImages(data);
-
-
-            }
-        });
+  $.ajax({
+    type: "GET",
+    dataType: 'json',
+    cache: false,
+    url: url,
+    success: function (data) {
+      var data =(data.Results);
+                //console.log(data.length);
+      for(i=0; i < data.length; i++) {
+        recipeArray.push(data[i])
       }
-
-
-    var renderImages = function(recipes) {
-    recipes.forEach(function (image) {
-      recipesArray.push(new Recipe(image.WebURL))
-    });
- }
-
-
-var Recipe = function(details){
-   this.url = details.ImageURL;
-   this.WebURL= details.WebURL;
-}
-
-getRecipeJson('pizza');
-
-
-
-console.log(recipesArray);
-
+    }
   });
+}
+var recipes = getRecipeJson('pizza');
+
+
+
+
