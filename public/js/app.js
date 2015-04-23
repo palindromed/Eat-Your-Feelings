6ad3c0d293f64f423053db3f6e85e3a1d996a661
Hannah Krager <hannahkrager@gmail.com>
2015-04-22 18:23:42 -0700

    broke image render

--- public/js/app.js
+++ public/js/app.js
@@ -170,13 +170,13 @@ var render = function(){
   };
 
   var picks = function(){
-    $('#contains').replaceWith('<section class="recipes"><figure class="four columns"><img src=" '+recipeArray[TL].image +'" \
-     id="outputs"/><figcaption>' + recipeArray[TL].name +'</figcaption></figure>\
-    <figure class="four columns"><img src="'+ recipeArray[TR].image + '" id="outputs"/><figcaption>'+ recipeArray[TR].name+
-    '</figcaption></figure></section><section class="recipes"><figure class="four columns"><img src=" '+recipeArray[BL].image +'" \
-     id="outputs"/><figcaption>' + recipeArray[BL].name +'</figcaption></figure>\
-    <figure class="four columns"><img src="'+ recipeArray[BR].image + '" id="outputs"/><figcaption>'+ recipeArray[BR].name+
-    '</figcaption></figure></section>')
+    $('#contains').replaceWith('<section class="recipes"><figure class="four columns">\
+      <img src=" '+recipeArray[TL].image +'"id="outputs"/><figcaption>' + recipeArray[TL].name +
+      '</figcaption></figure><figure class="four columns"><img src="'+ recipeArray[TR].image +
+      '" id="outputs"/><figcaption>'+ recipeArray[TR].name +'</figcaption></figure></section>\
+      <section class="recipes"><figure class="four columns"><img src=" '+recipeArray[BL].image +
+      '"id="outputs"/><figcaption>' + recipeArray[BL].name +'</figcaption></figure><figure class="four columns"><img src="'+ recipeArray[BR].image +
+      '" id="outputs"/><figcaption>'+ recipeArray[BR].name + '</figcaption></figure></section>')
   }
 
   var TL =randomize(0, recipeArray.length);
